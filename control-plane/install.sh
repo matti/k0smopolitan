1369f01dd4b0f00cd38f529ad44d92ab32f304dc
Matti Paksula <matti.paksula@iki.fi>
2024-07-18 19:15:47 +0300

    wat

--- control-plane/install.sh
+++ control-plane/install.sh
@@ -33,4 +33,15 @@ network:
 
 printf "@reboot screen -dmS k0s bash -l -c '/root/k0smopolitan/control-plane/start.sh'\n" | crontab
 
+
+rm -rf /etc/k0s
+rm -rf /var/lib/k0s
+
+if [[ -f /tmp/kubeconfig ]]; then
+  rm -rf /tmp/kubeconfig
+fi
+if [[ -f /tmp/jointoken ]]; then
+  rm -rf /tmp/jointoken
+fi
+
 reboot
