cfa69abb655bad7c8c3e4172d80b7cb5d9336b71
Matti Paksula <matti.paksula@iki.fi>
2024-07-18 18:32:04 +0300

    kk

--- worker/start.sh
+++ worker/start.sh
@@ -10,6 +10,21 @@ hostname_ipaddress="${ip_address//./-}"
 
 hostnamectl set-hostname "${hostname_now}-${hostname_ipaddress}"
 
+rm -rf /mnt/ramdisks
+mkdir -p /mnt/ramdisks
+
+ramdisk_size_total_mb=$(( 12 * 1024 ))
+ramdisk_size_usable_mb=$(( ramdisk_size_total_mb - 10 ))
+
+mount -t tmpfs -o size="${ramdisk_size_total_mb}m" tmpfs /mnt/ramdisks
+qemu-img create -o preallocation=full -f qcow2 /mnt/ramdisks/var-lib-k0s-containerd.qcow2 "${ramdisk_size_usable_mb}m"
+qemu-nbd --connect=/dev/nbd0 /mnt/ramdisks/var-lib-k0s-containerd.qcow2
+
+mkfs.ext4 /dev/nbd0
+
+mkdir -p /var/lib/k0s/containerd
+mount /dev/nbd0 /var/lib/k0s/containerd
+
 until [[ -f "$HOME/jointoken" ]] do
   echo "still waiting for jointoken..."
   sleep 1
