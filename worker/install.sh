6a6267609757e4772d20e663ce7b338205ba58a2
Matti Paksula <matti.paksula@iki.fi>
2025-07-26 16:14:02 +0300

    bump

--- worker/install.sh
+++ worker/install.sh
@@ -11,7 +11,7 @@ apt-get upgrade -y
 apt-get install -y --no-install-recommends \
   curl screen htop cron qemu-utils
 
-curl -sSLf https://get.k0s.sh | K0S_VERSION=v1.29.6+k0s.0 sh
+curl -sSLf https://get.k0s.sh | K0S_VERSION=v1.33.3+k0s.0 sh
 
 printf "@reboot screen -dmS k0s bash -l -c '/root/k0smopolitan/worker/edge.sh'\n" | crontab
 
