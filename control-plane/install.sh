e2f26363360aafd197407fd05d6ede1928b453d1
Matti Paksula <matti.paksula@iki.fi>
2025-07-26 09:20:57 +0300

    remove net from control-plane

--- control-plane/install.sh
+++ control-plane/install.sh
@@ -5,36 +5,15 @@ set -x
 
 export DEBIAN_FRONTEND=noninteractive
 
-gateway=$1
-ip_address=$2
-
 apt-get update
 apt-get upgrade -y
 
 apt-get install -y --no-install-recommends \
   curl screen htop cron
 
-curl -sSLf https://get.k0s.sh | K0S_VERSION=v1.29.6+k0s.0 sh
-
-echo """
-# k0smopolitan
-network:
-  ethernets:
-    ens18:
-      addresses:
-        - ${ip_address}/24
-      routes:
-        - to: default
-          via: ${gateway}
-      nameservers:
-        addresses:
-          - 1.1.1.1
-          - 8.8.8.8
-  version: 2
-""" > /etc/netplan/50-cloud-init.yaml
+curl -sSLf https://get.k0s.sh | K0S_VERSION=v1.33.3+k0s.0 sh
 
 printf "@reboot screen -dmS k0s bash -l -c '/root/k0smopolitan/control-plane/start.sh'\n" | crontab
-
 
 rm -rf /etc/k0s
 rm -rf /var/lib/k0s
